8b848edb560f0979acdcd00ad2fc4dcf7664eea5
MaxouMiles-tech <66181773+MaxouMiles-tech@users.noreply.github.com>
2020-10-27 16:18:22 +0100

    first commit  CV
    first commit

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-11">
+    <meta name="viewport" content="width=device-width, initial-scale=-2.0">
+    <title>Boussouf Zohra</title>
+    <!--CSS -->
+    <link rel="stylesheet" href='public/css/app.css'/>
+    <!-- Bootstrap CSS -->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+
+<body class="">
+<!-- header -->
+<div class="container-fluid">
+    <div class="row">
+        <div class=" jumbotron p-0 m-0">
+            <div  class="p-0 m-0 card-img-overlay d-lg-flex flex-column flex-column align-items-center justify-content-center text-center">
+                <div id="TitreVideo" class=" p-0 pb-2 m-0 ">
+                    <h1 class="font-weight-bold display-3">Boussouf Zohra</h1>
+                    <p class="lead font-weight-bold">Concepteur / Développeur d'applications Web</p>
+                    <a id="btnCTA1" class="btn btn-lg" href="#" role="button">Découvrir              mon profil</a>
+                </div>
+            </div>
+
+            <div>
+                <video autoplay muted loop>
+                    <source src="public/images/banniere.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
+    </div>
+</div>
+
