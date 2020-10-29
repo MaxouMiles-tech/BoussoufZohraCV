69ee8dfe9613f1586eec38401b7514bbdf01e70f
MaxouMiles-tech <66181773+MaxouMiles-tech@users.noreply.github.com>
2020-10-29 16:38:45 +0100

    about me section

--- header.php
+++ header.php
@@ -3,16 +3,31 @@
 
 <head>
     <!-- Required meta tags -->
-    <meta charset="UTF-11">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=-2.0">
     <title>Boussouf Zohra</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <!--CSS -->
     <link rel="stylesheet" href='public/css/app.css'/>
     <link rel="stylesheet" href='public/css/media.css'/>
     <link rel="stylesheet" href='public/css/colors.css'/>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<!--Favicon-->
+    <link rel="apple-touch-icon" sizes="57x57" href="public/images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="public/images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="public/images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="public/images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="public/images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="public/images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="public/images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="public/images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="public/images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="public/images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="public/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="public/images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="public/images/favicon/favicon-16x16.png">
 </head>
 
 <body>
@@ -30,35 +45,36 @@
 <!--video de l'entete-->
     <div id="videoHome">
         <video  autoplay muted loop>
-            <source src="public/images/banniere.mp4" type="video/mp4">
+            <source src="public/videos/banniere.mp4" type="video/mp4">
         </video>
     </div>
-<!--barre de navigation-->
-    <nav class=" mt-n2 navbar navbar-expand-md navbar-dark color-primary-0 " id="navbarTop">
-        <a class="navbar-brand pl-3" href="index.php">ZB</a>
-        <button id="btnmenu" class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNav">
+    <!--Barre de navigation -->
+    <nav class="navbar navbar-dark navbar-expand-md color-primary-0 mt-n2  " id="navbarTop">
+        <a href="index.php"  class="navbar-brand d-flex w-50 mr-auto pl-3 ">
+            <img src="public/images/favicon/favicon.png" alt="Favicon ZB" id="faviconZB">
+        </a>
+        <button id="btnmenu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbarTop">
             <span class="navbar-toggler-icon" id="togglerIcon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-center" id="collapsibleNav">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Qui suis-je?<span class="sr-only">(current)</span></a>
+        <div class="navbar-collapse collapse w-100 " id="collapsingNavbarTop">
+            <ul class="nav navbar-nav ml-auto w-100 justify-content-end ">
+                <li class="nav-item active mr-5">
+                    <a class="nav-link" href="quiSuisJe.php">Qui suis-je?<span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item mr-5">
                     <a class="nav-link" href="#">Compétences</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item mr-5">
                     <a class="nav-link" href="#">Expériences</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item mr-5">
                     <a class="nav-link" href="#">Ce qui m'anime</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item mr-5">
                     <a class="nav-link" href="#">Contact</a>
                 </li>
             </ul>
         </div>
     </nav>
-
 
 </div>
---
+++ quiSuisJe.php
@@ -0,0 +1,39 @@
+<div class="row  justify-content-around">
+    <div class="col-md-6 m-auto" id="backJaune">
+        <div class="about-img">
+            <img src="public/images/moi.jpg" alt="Photo de montagne" id="moi">
+        </div>
+
+    </div>
+
+    <div class="col-md-6 pt-5">
+        <div class="about-item">
+            <div class="about-title">
+                <h2 id="tiret">Qui suis-je ?</h2>
+            </div>
+            <div class="about-content">
+                <h2>Développeur / concepteur d'applications Web</h2>
+                <p> Diplômée en chimie organique et passionnée par le Digital. Je suis dynamique, rigoureuse et
+                    spontanée. </p>
+
+                <p> De part mes différentes expériences dans l’économie sociale et solidaire, j'ai beaucoup été amenée à
+                    intervenir
+                    au niveau de la <strong>communication</strong> et de la <strong>gestion de projet</strong>.</p>
+
+                <p> Aujourd'hui je suis prête pour de <strong>nouvelles aventures</strong> !</p>
+
+                <p> En reconversion professionnelle, je recherche un stage <strong>non remunénéré</strong> en tant que
+                    <strong>Concepteur/ Développeur Web</strong>
+                    après une première formation en tant que développeuse web. Une façon, pour moi de combiner mes
+                    compétences scientifiques et mon appétence pour le monde du numérique.
+                </p>
+                <p>Mon ambition est d'associer design, communication et technologie, afin d'accompagner les porteurs de
+                    projets lors de leurs transformation digitale : mise en oeuvre, conception, réalisation, gestion et
+                    mesure de la performance.</p>
+            </div>
+        </div>
+        <!--    <hr id="quisuisje">-->
+
+
+    </div>
+</div>
