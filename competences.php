b6acd2fa737194e572158cf7ffa7d3dceabbafbe
Zohra Boussouf <boussouf.zohra@gmail.com>
2020-11-11 21:13:43 +0100

    ajout btn

--- competences.php
+++ competences.php
@@ -8,60 +8,63 @@
 </div>
 <div class="row">
     <div class="col-lg-6 selector">
-        <div class="d-flex justify-content-center">
-            <img src="public/images/competence/titre.png" alt="Logo Zohra Boussouf" id="logoZB">
-        </div>
-        <ul class="list-unstyled d-lg-flex flex-lg-column p-0 m-0">
-            <li class="d-flex justify-content-center">
-                <figure class="figure text-center">
-                    <img src="public/images/competence/apprentissage.png" alt="apprentissage" id="apprentissage">
-                    <figcaption class="figure-caption text-center">Capacité d'apprentissage</figcaption>
-                </figure>
-            </li>
-            <li class="d-flex justify-content-center">
-                <figure class="figure text-center">
-                    <img src="public/images/competence/communication.png" alt="communication" id="communication">
-                    <figcaption class="figure-caption text-center">Communication</figcaption>
-                </figure>
-            </li>
-            <li class="d-flex justify-content-center">
-                <figure class="figure text-center">
-                    <img src="public/images/competence/reflexion.png" alt="réflexion" id="réflexion">
-                    <figcaption class="figure-caption text-center">Réflexion</figcaption>
-                </figure>
-            </li>
-            <li class="d-flex justify-content-center">
-                <figure class="figure text-center">
-                    <img src="public/images/competence/analyse.png" alt="capacité d'analyse" id="analyse">
-                    <figcaption class="figure-caption text-center">Capacité d'analyse</figcaption>
-                </figure>
-            </li>
-            <li class="d-flex justify-content-center">
-                <figure class="figure text-center">
-                    <img src="public/images/competence/planification.png" alt="planification" id="planification">
-                    <figcaption class="figure-caption text-center">Planification</figcaption>
-                </figure>
-            </li>
-            <li class="d-flex justify-content-center">
-                <figure class="figure text-center">
-                    <img src="public/images/competence/time.png" alt="patience" id="patience">
-                    <figcaption class="figure-caption text-center">Patience</figcaption>
-                </figure>
-            </li>
-            <li class="d-flex justify-content-center">
-                <figure class="figure text-center">
-                    <img src="public/images/competence/connected-people.png" alt="esprit d'équipe" id="equipe">
-                    <figcaption class="figure-caption text-center">Esprit d'équipe</figcaption>
-                </figure>
-            </li>
-            <li class="d-flex justify-content-center">
-                <figure class="figure text-center">
-                    <img src="public/images/competence/language.png" alt="langues" id="langue">
-                    <figcaption class="figure-caption text-center">Langues</figcaption>
-                </figure>
-            </li>
-        </ul>
 
+            <button type="button" class="btn" id="btnZB"></button>
+<div class="skills-item" id="skills-item">
+    <ul class="list-unstyled d-lg-flex flex-lg-column p-0 m-0">
+        <li class="d-flex justify-content-center">
+            <figure class="figure text-center ">
+                <img src="public/images/competence/apprentissage.png" alt="apprentissage" id="apprentissage">
+                <figcaption class="d-md-block d-none figure-caption text-center">Capacité d'apprentissage</figcaption>
+            </figure>
+        </li>
+        <li class="d-flex justify-content-center">
+            <figure class="figure text-center">
+                <img src="public/images/competence/communication.png" alt="communication" id="communication">
+                <figcaption class="d-md-block d-none figure-caption text-center">Communication</figcaption>
+            </figure>
+        </li>
+        <li class="d-flex justify-content-center">
+            <figure class="figure text-center">
+                <img src="public/images/competence/reflexion.png" alt="réflexion" id="réflexion">
+                <figcaption class="d-md-block d-none figure-caption text-center">Réflexion</figcaption>
+            </figure>
+        </li>
+        <li class="d-flex justify-content-center">
+            <figure class="figure text-center">
+                <img src="public/images/competence/analyse.png" alt="capacité d'analyse" id="analyse">
+                <figcaption class="d-md-block d-none figure-caption text-center">Capacité d'analyse</figcaption>
+            </figure>
+        </li>
+        <li class="d-flex justify-content-center">
+            <figure class="figure text-center">
+                <img src="public/images/competence/planification.png" alt="planification" id="planification">
+                <figcaption class="d-md-block d-none figure-caption text-center">Planification</figcaption>
+            </figure>
+        </li>
+        <li class="d-flex justify-content-center">
+            <figure class="figure text-center">
+                <img src="public/images/competence/time.png" alt="patience" id="patience">
+                <figcaption class="d-md-block d-none figure-caption text-center">Patience</figcaption>
+            </figure>
+        </li>
+        <li class="d-flex justify-content-center">
+            <figure class="figure text-center">
+                <img src="public/images/competence/connected-people.png" alt="esprit d'équipe" id="equipe">
+                <figcaption class="d-md-block d-none figure-caption text-center">Esprit d'équipe</figcaption>
+            </figure>
+        </li>
+        <li class="d-flex justify-content-center">
+            <figure class="figure text-center">
+                <img src="public/images/competence/language.png" alt="langues" id="langue">
+                <figcaption class="d-md-block d-none figure-caption text-center">Langues</figcaption>
+            </figure>
+        </li>
+    </ul>
+
+
+</div>
+        <div id="cn-overlay" class="cn-overlay"></div>
 
 
     </div>
