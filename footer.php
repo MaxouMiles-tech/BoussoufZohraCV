09d178d93093617a550129d7141c379a40ed955c
Zohra Boussouf <boussouf.zohra@gmail.com>
2020-11-21 10:02:53 +0100

    ajout index

--- footer.php
+++ footer.php
@@ -22,7 +22,7 @@
                 <img id="github" class="social rounded" src="public/images/icoGithub.png" alt="Icone Github"></a>
             <a  class="mr-md-3" href="public/CVZohraBoussouf.pdf" target="_blank" title="CV PDF">
                 <img id="cv" class="social rounded" src="public/images/icoCv.png" alt="Icone téléchargement CV"></a>
-            <a href="https://dev.amorce.org/boussoufz/" target="_blank" title="Portfolio">
+            <a href="portfolio.php" target="_blank" title="Portfolio">
                 <img id="cv" class="social rounded" src="public/images/portfolio.png" alt="Icone Portfolio"></a>
         </div>
     </nav>
---
+++ portfolio.php
@@ -0,0 +1,85 @@
+<!--------------------------------------------------------------------------------------------------------------------->
+<!--Portfolio-->
+<!--------------------------------------------------------------------------------------------------------------------->
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Zohra Boussouf</title>
+    <!-- <link rel="icon" href="img/favicon.png" /> -->
+    <!-- Bootstrap CSS -->
+    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/portfolio.css">
+</head>
+
+<body>
+<!-- corps du site  -->
+<div class="container-fluid min-vh-100 d-flex flex-column ">
+    <div class="d-flex justify-content-center row site-header ">
+        <!-- banniere -->
+        <div class="d-flex justify-content-center" id="banniere">
+            <img class="img-fluid" src="public/images/portfolio/logozb.png" alt="Zohra Boussouf" id="logozb">
+        </div>
+    </div>
+    <div class="row d-flex flex-wrap align-items-center justify-content-center" id="carte">
+        <div class="col-12 col-md-3 d-flex justify-content-center justify-content-md-end">
+            <div class="card ">
+                <img class="card-img-top" src="public/images/portfolio/jarditou.png" alt="Photo représentant un e-commerce">
+                <div class="card-body text-center">
+                    <h2 class="card-title">Jarditou</h2> <hr>
+                    <p class="card-text mt-2">
+                        Projet AFPA techniques de base 20103 : création du site Web Jarditou suivant une maquette.
+                    </p>
+                    <a href="https://dev.amorce.org/boussoufz/Jarditou/" target="_blank" class="btn mt-2 p-2">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-3 d-flex justify-content-center  justify-content-md-center">
+            <div class="card mt-md-0 mt-5">
+                <img class="card-img-top" src="public/images/portfolio/cvzb.png" alt="Photo représentant un CV">
+                <div class="card-body text-center">
+                    <h2 class="card-title">Mon CV</h2> <hr>
+                    <p class="card-text mt-2">
+                        Projet AFPA techniques de base 20103 : création d'un CV en ligne (première version).
+                    </p>
+                    <a href="https://dev.amorce.org/boussoufz/zohracv/" target="_blank" class="btn mt-2 p-2">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="col col-md-3 d-flex justify-content-center  justify-content-md-start">
+            <div class="card mt-md-0 mt-5">
+                <img class="card-img-top" src="public/images/portfolio/photo.png" alt="Photo représentant de la photographie">
+                <div class="card-body text-center">
+                    <h2 class="card-title">Photographie</h2> <hr>
+                    <p class="card-text mt-2">
+                        Projet personnel : création de la maquette et d'un site Web pour un photographe professionnel.
+                    </p>
+                    <a href="#" class="btn mt-2 p-2">Voir le site</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Optional JavaScript-->
+
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+
+
+</body>
+</html>
